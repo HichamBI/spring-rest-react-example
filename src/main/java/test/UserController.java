98a9ed13b1e7e24b3fa9ebe44aec0a1a8cf2f7e9
Hei <hicham.bouzidi@gmail.com>
2015-02-15 22:50:06 +0100

    clean UserController + add gitignore

--- src/main/java/test/UserController.java
+++ src/main/java/test/UserController.java
@@ -5,25 +5,22 @@
 package test;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 @RequestMapping("user")
 public class UserController {
-    private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/all")
-    public Collection<User> test(@RequestParam(value = "name", defaultValue = "World") String name) {
+    public Collection<User> getUsers() {
         return new ArrayList<User>(
                 Arrays.asList(
-                        new User(counter.incrementAndGet(), "Hei"),
-                        new User(counter.incrementAndGet(), "BK-201")
+                        new User(1, "Hei"),
+                        new User(2, "BK-201")
                 ));
     }
 }
